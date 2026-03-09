565ca8ba5b7b7c0a2cd4304a0fa2ed58692a244e
lahirua-madhusanka <lahirua.madhusanka@gmail.com>
2026-03-09 16:25:33 +0530

    welcome page

--- app/src/main/java/com/mad/cw/MainActivity.java
+++ app/src/main/java/com/mad/cw/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mad.cw;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // we can use sharedPreference to save states of the user and check if the user is logged in or not.
+        // for now, we will just use a boolean variable to check if the user is logged in or not.
+        boolean isLoggedIn = false;
+
+        if(isLoggedIn){
+            Intent intent = new Intent(MainActivity.this, Welcome.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            long splashTime = 4500L;
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, Welcome.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, splashTime);
+        }
     }
 }
